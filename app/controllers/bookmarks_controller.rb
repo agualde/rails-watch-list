2ab367a15cbc041b6f5ea348fd315efb77b46888
Andres Gualde <andres@gualde.com>
2022-02-18 11:44:06 +0100

    Footer stuck to the bottom now and NavBar fixed Z axis

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -32,7 +32,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    redirect_to list_path(@bookmark.list_id)
   end
 
   private
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.all
   end
 
   def new
