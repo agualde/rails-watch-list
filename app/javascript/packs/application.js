01af33aae881821c17f18598c9e736982309fec5
Andres Gualde <andres@gualde.com>
2022-02-20 02:09:20 +0100

    meaningful message

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,14 +10,6 @@ import "channels"
 import "bootstrap";
 require("@fortawesome/fontawesome-free")
 
-import { Application } from "stimulus"
-import { autoload } from "stimulus/webpack-helpers"
-
-// Look for controllers inside app/javascripts/packs/controllers/
-const application = Application.start()
-const controllers = require.context("./controllers", true, /\.js$/)
-autoload(controllers, application)
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
